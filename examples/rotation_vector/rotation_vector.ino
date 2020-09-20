c656229dfc6ce450ee7651aa644bcdbb645c811c
lady ada <limor@ladyada.net>
2020-09-20 00:24:19 -0400

    add example

--- examples/rotation_vector/rotation_vector.ino
+++ examples/rotation_vector/rotation_vector.ino
@@ -1,19 +1,15 @@
-// Basic demo for accelerometer readings from Adafruit BNO08x
-
-
 // Basic demo for readings from Adafruit BNO08x
-#include <Wire.h>
 #include <Adafruit_BNO08x.h>
-#include <Adafruit_Sensor.h>
 
 // For SPI mode, we need a CS pin
 #define BNO08X_CS 10
-// For software-SPI mode we need SCK/MOSI/MISO pins
-#define BNO08X_SCK 13
-#define BNO08X_MISO 12
-#define BNO08X_MOSI 11
+#define BNO08X_INT 9
 
-Adafruit_BNO08x  bno08x; // TODO FIX NAME
+#define BNO08X_RESET 5
+
+Adafruit_BNO08x  bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
 void setup(void) {
   Serial.begin(115200);
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
@@ -22,16 +18,65 @@ void setup(void) {
 
   // Try to initialize!
   if (!bno08x.begin_I2C()) {
-  //if (!bno08x.begin_SPI(BNO08X_CS)) {
-  //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_SCK, BNO08X_MISO, BNO08X_MOSI)) {
+  //if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
+  //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
     Serial.println("Failed to find BNO08x chip");
     while (1) { delay(10); }
   }
   Serial.println("BNO08x Found!");
+
+  for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
+    Serial.print("Part ");
+    Serial.print(bno08x.prodIds.entry[n].swPartNumber);
+    Serial.print(": Version :");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMajor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionMinor);
+    Serial.print(".");
+    Serial.print(bno08x.prodIds.entry[n].swVersionPatch);
+    Serial.print(" Build ");
+    Serial.println(bno08x.prodIds.entry[n].swBuildNumber);
+  }
+
+  Serial.println("Setting desired reports");
+  setReports();
+
+  Serial.println("Reading events");
+  delay(100);
+}
+
+// Here is where you define the sensor outputs you want to receive
+void setReports(void) {
+  if (! bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+    Serial.println("Could not enable game vector");
+  }
 }
 
 
 void loop() {
-
   delay(10);
+
+  if (bno08x.wasReset()) {
+    Serial.println("Setting desired reports");
+    setReports();
+  }
+  
+  if (! bno08x.getSensorEvent(&sensorValue)) {
+    return;
+  }
+  
+  switch (sensorValue.sensorId) {
+    
+    case SH2_GAME_ROTATION_VECTOR:
+      Serial.print("Game Rotation Vector - r: ");
+      Serial.print(sensorValue.un.gameRotationVector.real);
+      Serial.print(" i: ");
+      Serial.print(sensorValue.un.gameRotationVector.i);
+      Serial.print(" j: ");
+      Serial.print(sensorValue.un.gameRotationVector.j);
+      Serial.print(" k: ");
+      Serial.println(sensorValue.un.gameRotationVector.k);
+      break;
+  }
+
 }
