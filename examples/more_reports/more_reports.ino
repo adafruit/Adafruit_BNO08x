4cbdbd028ae43d38e10e2bd4d880c5406cb3c9e4
Kevin Gilliam <10750198+BangoDurango@users.noreply.github.com>
2021-03-06 18:45:16 -0700

    added gravity report to more_reports.ino

--- examples/more_reports/more_reports.ino
+++ examples/more_reports/more_reports.ino
@@ -65,6 +65,9 @@ void setReports(void) {
   if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION)) {
     Serial.println("Could not enable linear acceleration");
   }
+  if (!bno08x.enableReport(SH2_GRAVITY)) {
+    Serial.println("Could not enable gravity vector");
+  }
   if (!bno08x.enableReport(SH2_ROTATION_VECTOR)) {
     Serial.println("Could not enable rotation vector");
   }
@@ -177,6 +180,14 @@ void loop() {
     Serial.print(sensorValue.un.linearAcceleration.y);
     Serial.print(" z: ");
     Serial.println(sensorValue.un.linearAcceleration.z);
+    break;
+  case SH2_GRAVITY:
+    Serial.print("Gravity - x: ");
+    Serial.print(sensorValue.un.gravity.x);
+    Serial.print(" y: ");
+    Serial.print(sensorValue.un.gravity.y);
+    Serial.print(" z: ");
+    Serial.println(sensorValue.un.gravity.z);
     break;
   case SH2_ROTATION_VECTOR:
     Serial.print("Rotation Vector - r: ");
